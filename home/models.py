6e10d95078bb73feb9a9caf6e95ba7d5527f8c5b
strangersofficial <strangersofficial517@gmail.com>
2024-11-29 18:20:16 +0000

    update product model

--- home/models.py
+++ home/models.py
@@ -9,7 +9,14 @@ class Product(models.Model):
     image = models.ImageField(upload_to='strangers/productImages/',null=True,blank=True)
     category = models.CharField(max_length=255,null=True,blank=True)
     color_tag = models.CharField(max_length=255,null=True,blank=True)
-
+    description = models.TextField(null=True,blank=True)
+    product_ordering = models.PositiveIntegerField(null=True,blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
+    stock = models.PositiveIntegerField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
+    is_active = models.BooleanField(default=True)
+    size_av = models.BooleanField(default=True)
     
     def __str__(self):
         color_tag = self.color_tag
