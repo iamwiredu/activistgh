6b10bda4cbf56c084302b245240945f9954f0e1b
strangersofficial <strangersofficial517@gmail.com>
2024-11-29 08:59:50 +0000

    first commit

---
+++ home/models.py
@@ -0,0 +1,22 @@
+import uuid
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    unique_id = models.UUIDField(editable=False,unique=True,default=uuid.uuid4)
+    image_url = models.CharField(max_length=255)
+    category = models.CharField(max_length=255,null=True,blank=True)
+    color_tag = models.CharField(max_length=255,null=True,blank=True)
+
+    
+    def __str__(self):
+        color_tag = self.color_tag
+        if color_tag:
+            return f'{self.name}  {color_tag} Category:{self.category}' 
+        else:
+            color_tag = ''
+            return f'{self.name}  {color_tag} Category:{self.category}' 
+        
+            
