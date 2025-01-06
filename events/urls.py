b3116844eb67e024137684e22a983d421c8badb7
strangersofficial <strangersofficial517@gmail.com>
2025-01-06 16:52:42 +0000

    remove events

--- events/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Events, EventDetailView
-
-
-urlpatterns = [
-    # path('event/<str:unique_id>/',events,name='event'),
-    path('events/',Events.as_view(),name='events'),
-    path('eventdetail/<str:unique_id>/',EventDetailView.as_view(),name='event')
-]
--- strangers/urls.py
+++ strangers/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('',include('events.urls')),
     path('',include('userAdmin.urls'))
 
 ]
