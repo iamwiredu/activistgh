fac04096b9974d396be7b179088fcf8f810384fd
strangersofficial <strangersofficial517@gmail.com>
2025-02-20 01:08:14 +0000

    current

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ from userAdmin.models import Notification
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
