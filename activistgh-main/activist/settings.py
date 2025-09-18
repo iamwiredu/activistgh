3d2d5e212fb75652d3e946aa7e69e053c9e9bb9b
activistgh <activistgh@gmail.com>
2025-09-18 17:41:58 +0000

    change

--- activistgh-main/activist/settings.py
+++ activistgh-main/activist/settings.py
@@ -142,27 +142,27 @@ USE_TZ = True
 
 
 
-#Google Cloud Storage settings
-GS_PROJECT_ID = 'precise-line-437900-m9'
-GS_BUCKET_NAME = 'activistimages'
+# #Google Cloud Storage settings
+# GS_PROJECT_ID = 'precise-line-437900-m9'
+# GS_BUCKET_NAME = 'activistimages'
 
-# Get the JSON key data from environment variable
-google_cloud_key = os.getenv('KEY')
+# # Get the JSON key data from environment variable
+# google_cloud_key = os.getenv('KEY')
 
-# Parse the JSON key
-google_cloud_info = json.loads(google_cloud_key)
+# # Parse the JSON key
+# google_cloud_info = json.loads(google_cloud_key)
 
-# settings.py
-DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
-    google_cloud_info
-)
+# # settings.py
+# DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+# GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
+#     google_cloud_info
+# )
 
-# Media files (uploads)
-GS_MEDIA_BUCKET_NAME = GS_BUCKET_NAME
-MEDIA_URL = f'https://storage.googleapis.com/{GS_MEDIA_BUCKET_NAME}/'
+# # Media files (uploads)
+# GS_MEDIA_BUCKET_NAME = GS_BUCKET_NAME
+# MEDIA_URL = f'https://storage.googleapis.com/{GS_MEDIA_BUCKET_NAME}/'
 
-#update
+# #update
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
